1e96c5a4bf7cc6af06d6b2583a861a75e9463319
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-11-26 23:24:13 +0530

    import images to the gallery page

--- src/Pages/AboutMe/GallerySection.jsx
+++ src/Pages/AboutMe/GallerySection.jsx
@@ -1,12 +1,20 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { X } from "lucide-react";
-import img1 from "../../assets/projects/1.png";
-import img2 from "../../assets/bg2.jpg";
-import img3 from "../../assets/projects/3.png";
-import img4 from "../../assets/projects/4.png";
-import img5 from "../../assets/projects/4.png";
-import img6 from "../../assets/projects/3.png";
+import img1 from "../../assets/gallery/g1.jpeg";
+import img2 from "../../assets/gallery/g2.jpeg";
+import img3 from "../../assets/gallery/g3.jpeg";
+import img4 from "../../assets/gallery/g4.jpeg";
+import img5 from "../../assets/gallery/g5.jpeg";
+import img6 from "../../assets/gallery/g6.jpeg";
+import img7 from "../../assets/gallery/g7.jpeg";
+import img8 from "../../assets/gallery/g8.jpeg";
+import img9 from "../../assets/gallery/g9.jpeg";
+import img10 from "../../assets/gallery/g10.jpeg";
+import img11 from "../../assets/gallery/g11.jpeg";
+import img12 from "../../assets/gallery/g12.jpeg";
+import img13 from "../../assets/gallery/g13.jpeg";
+import img14 from "../../assets/gallery/g14.jpeg";
 
 const GallerySection = () => {
     const [selectedImage, setSelectedImage] = useState(null);
